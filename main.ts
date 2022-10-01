65b359d5bc5ae90a7a61aec34ae3ff4d1bbb1d5d
javier-iesvae <114832717+javier-iesvae@users.noreply.github.com>
2022-10-02 00:01:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+let n = 0
+basic.forever(function () {
+    led.plotBarGraph(
+    n,
+    10
+    )
+    if (n > 10) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 500 || input.acceleration(Dimension.X) < -500) {
+        n += 1
+        basic.pause(200)
+    }
+})
